14725a2c47349bb0417e011e562cb42dfa5e63b8
Stanley Huang <huang.stanley050@gmail.com>
2020-01-21 20:57:28 +1100

    fetching after mutation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 import { Link, hashHistory } from "react-router";
+import query from "../queries/fetchSongs";
 
 class SongCreate extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ class SongCreate extends Component {
     e.preventDefault();
     this.props
       .mutate({
+        refetchQueries: [{ query }],
         variables: {
           title: this.state.title
         }
