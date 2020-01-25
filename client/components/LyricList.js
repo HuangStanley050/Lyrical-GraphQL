adca94920079689bfae7624ea0a1dfb70e114df3
Stanley Huang <huang.stanley050@gmail.com>
2020-01-25 12:36:17 +1100

    started with LyricList

---
+++ client/components/LyricList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class LyricList extends React.Component {
+  render() {
+    return <ul>lyric list</ul>;
+  }
+}
+
+export default LyricList;
--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,6 +3,7 @@ import { graphql } from "react-apollo";
 import fetchSong from "../queries/fetchSong";
 import { Link } from "react-router";
 import LyricCreate from "./LyricCreate";
+import LyricList from "./LyricList";
 
 const SongDetail = props => {
   const { song } = props.data;
@@ -13,6 +14,7 @@ const SongDetail = props => {
     <div>
       <Link to="/">Back</Link>
       <h3>{song.title}</h3>
+      <LyricList />
       <LyricCreate songId={props.params.id} />
     </div>
   );
